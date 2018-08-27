2b79bc743d84de9cf0f373f19d246bba159d9fd2
talagluck <michaellgl@gmail.com>
2018-08-27 16:19:01 -0400

    another commit

--- hanabi2.py
+++ hanabi2.py
@@ -1,7 +1,6 @@
 from random import shuffle
 # from collections import counter
 
-
 class Tile(object):
     def __init__(self,color,value):
         if color not in Deck.tile_colors:
@@ -11,7 +10,7 @@ class Tile(object):
         self.color = color
         self.value = int(value)
         self.name = f'{self.color},{self.value}'
-        self.notes = ''
+        self.notes = []
 
     def __repr__(self):
         return f'{self.color},{self.value}'
@@ -24,9 +23,8 @@ class Deck(object):
     def __init__(self):
         for color in Deck.tile_colors:
             for key, value in Deck.tile_values.items():
-                # print(color,value)
-                tile = Tile(color, key)
                 for i in range(0,value):
+                    tile = Tile(color, key)
                     Deck.tiles.append(tile)
         shuffle(Deck.tiles)
 
@@ -65,57 +63,63 @@ class Deck(object):
 
     def deal_tile(self):
         return self._deal(1)
-#
-# class Hand(dict):
-#     def __init__(self, dealt):
-#         self.hand = [[{'label':'a'}],
-#                     [{'label':'b'}],
-#                     [{'label':'c'}],
-#                     [{'label':'d'}]]
-#
-#         for i, position in enumerate(self.hand):
-#             position.append(dealt[i])
-#
-#     def __repr__(self):
-#         return self.hand
 
 
 class Player(object):
-    def __init__(self, deck, name):
-        # self.hand = Hand(deck.deal_hand()).hand
+    index_letters ={0:'a',1:'b',2:'c',3:'d'}
+    player_number_range=[]
+    def __init__(self, deck, name, number):
+        self.tile_number = 10
+        self.name = name
+        self.number = number
+        Player.player_number_range.append(self.number)
+        self.possible_hints={'color':set([]),'value':set([])}
         self.hand = [[{'label':'a'}],
                     [{'label':'b'}],
                     [{'label':'c'}],
                     [{'label':'d'}]]
 
-        self.name = name
-        dealt = deck.deal_hand()
-        i=0
+        dealt = deck.deal_hand() #create initial hand and deal it out
+        # i=0
         for i, position in enumerate(self.hand):
             position.append(dealt[i])
-            i+=1
+            # print(self.name,self.tile_number,position[1].notes, position[1])
+            self.add_number_to_notes(i)
+
+
+        self.populate_hints()
 
     def __repr__(self):
         # return str(self.hand)
         return self.name
-    #
-    # def parse_tile(self, tile):
-    #     self.parse_color,self.parse_value = tile.split(',')
-    #     self.parse_value = int(self.parse_value)
+
+    def add_number_to_notes(self,tile_index):
+        tile_to_note = self.hand[tile_index][1]
+        tile_to_note.notes.append(self.tile_number)
+        self.tile_number+=1
+
+    def populate_hints(self):
+        for tile in self.hand:
+            self.possible_hints['color'].add(tile[1].color)
+            self.possible_hints['value'].add(tile[1].value)
 
     def give_tile(self, tile_letter):
         given_tile = [tile for tile in self.hand if tile[0]['label'] == tile_letter][0]
         given_tile_index = self.hand.index(given_tile)
         given_tile = self.hand[given_tile_index].pop(1) #remove the actual played tile from the hand
+        self.possible_hints['color'].clear()
+        self.possible_hints['value'].clear()
         return given_tile, given_tile_index
 
     def receive_tile(self, deck, idx, letter):
         new_dealt_tile = deck.deal_tile()[0]
         self.hand[idx].append(new_dealt_tile)
-        print(f'{self.name} drew a new tile to position {letter}')
+        self.add_number_to_notes(idx)
+        self.populate_hints()
+        print(f'{self.name} drew a new tile to position {Player.index_letters[idx]}')
 
     def play_tile(self, tile_letter, deck, board):
-        print(self.hand)
+        # print(self.hand)
         played_tile, played_tile_index = self.give_tile(tile_letter)
 
         # self.parse_tile(played_tile['hidden']) #get parse_color and parse_value
@@ -137,12 +141,8 @@ class Player(object):
             board.explosions-=1
             print(f'''BOOM!\nThere was no available place on the board for the {played_tile.color} {played_tile.value}.\nYou have {board.explosions} tries remaining!''')
 
-        #deal a new tile
-        # new_dealt_tile = {'known':'','hidden':deck.deal_tile()[0]}
-        # self.hand[played_tile_index].append(new_dealt_tile)
-        # print(f'{self.name} drew a new tile to position {tile_letter}')
         self.receive_tile(deck, played_tile_index, tile_letter)
-        print(self.hand)
+        # print(self.hand)
         return('')
 
 
@@ -151,38 +151,111 @@ class Player(object):
         discarded_tile, discarded_tile_index = self.give_tile(tile_letter)
 
         board.discard_pile.append(discarded_tile)
-        print(f'{self.name} discarded a {discarded_tile.color} {discarded_tile.value}.')
+        if board.hints < board.max_hints:
+            board.hints+=1
+        print(f'{self.name} discarded a {discarded_tile.color} {discarded_tile.value}, and gained back a hint.')
         print(f'Discard pile consists of {board.discard_pile}')
         self.receive_tile(deck, discarded_tile_index, tile_letter)
-        print(self.hand)
+        # print(self.hand)
         return('')
 
-    def give_hint(self,player,hint):
-        for tile in player.hand:
-            self.parse_tile(tile[1])
+    def give_hint(self,player,hint,board):
+        if board.hints>0:
+            for tile in player.hand:
+                if tile[1].color == hint:
+                    tile[1].notes.append(hint)
+                if tile[1].value == hint:
+                    tile[1].notes.append(hint)
+            board.hints-=1
+        else:
+            print("No hints remain. Please discard a tile to regain a hint, or play a tile.")
 
 
+    def take_turn(self, board): #can play a tile, discard a tile, or give a hint
+        while True:
+            print(f"It's {self.name}'s turn. You can:")
+            print("1. Give a hint")
+            print("2. Discard a tile")
+            print("3. Play a tile")
+            turnType = int(input("What'll it be?\n"))
+            if turnType not in [1,2,3]:
+                print("Please enter 1, 2, or 3\n")
+            else:
+                if turnType == 1:
+                    while True:
+                        confirm = input("Are you sure you want to give a hint? y/n\n")
+                        if confirm not in ["y","n"]:
+                            print("Please enter y or n\n")
+                        else:
+                            while True:
+                                print("You can see the following players and their hands\n")
+                                for player in board.player_list:
+                                    print(player, player.hand)
+                                player_to_hint = input("To whom would you like to give a hint?\n")
+                                if player_to_hint not in board.player_name_list:
+                                    print("Not a valid player. Please enter a valid player.\n")
+                                else:
+                                    while True:
+                                        hint_nums = [1]
+                                        print("You can give the following hints:")
+# !!!!!! change player to player_to_hint - access player object through player_to_hint name
+                                        for hint_category, specific_hint in player.possible_hints.items():
+                                            for hint in specific_hint:
+                                                print(hint_nums[len(hint_nums)-1], hint)
+                                                hint_nums.append(hint_nums[len(hint_nums)-1]+1)
+                                        hint_to_give = input("Please enter the number of the hint you would like to give.\n")
+                                        if hint_to_give not in hint_nums:
+                                            print("Please enter a valid hint number.\n")
+                                        else:
+                                            self.give_hint(player, hint_to_give, board)
+                                            print(f"You told {player} about their {hint_to_give} tiles")
+                                            break
+                                    break
+                            break
+                    break
 
-    def take_turn(self, action): #can play a tile, discard a tile, or give a hint
-        pass
+                    self.give_hint()
 
-    def reorder_tiles(self, swap_from, swap_to):
-        pass
+                if turnType == 2:
+                    self.discard_tile()
+
+                if turnType == 3:
+                    self.play_tile()
+
+                break
+
+
 
 class Board(object):
-    max_hints = 8
 
     def __init__(self, deck):
+        self.max_hints = 8
         self.deck = deck
         self.discard_pile = []
         self.explosions = 3
         self.hints = self.max_hints
         self.board_state = {}
+        self.player_list=[]
+        self.player_name_list =[]
         for color in Deck.tile_colors:
             self.board_state[color]=0
 
     def __repr__(self):
         return f'''board: {self.board_state}\nexplosions remaining: {self.explosions}\nhints remaining: {self.hints}\ntiles remaining: {self.deck.count()}'''
+
+    def start_game(self,number_of_players):
+        for i in range(0,number_of_players):
+            name = input(f'Please enter the name of Player {i+1}')
+            player = Player(self.deck, name, i+1)
+            self.player_list.append(player)
+            self.player_name_list.append(player.name)
+
+        while True:
+            for player in self.player_list:
+                player.take_turn(self)
+
+
+
 
 #add methods/attributes to the Tile class:
 #color
@@ -193,29 +266,11 @@ class Board(object):
 # # tile = Tile('red','5')
 # # print(tile)
 d = Deck()
-for i in d:
-    print(i)
-# # print(d)
-# # h = Hand(d.deal_hand())
-# # h = h.hand
-# # print(h)
-# # print(type(h.hand))
 
-# # for i,j in h.hand.items():
-# #     print(i,j)
-# # print(h2)
-tom = Player(d, "tom")
-print(tom.hand)
+tom = Player(d, "tom",1)
 
-# ren = Player(d, "ren")
-# #
-# # # print(tom.known_hand)
-# # # print(ren.hand)
-# #
-b= Board(d)
-print(tom.play_tile('a',d,b))
-# # # print(b)
-print(tom.discard_tile('a',d,b))
-# print(tom.discard_tile('b',d,b))
-# print(tom.hidden_hand[0][1])
-# # print(b)
+ren = Player(d, "ren",2)
+# print(ren.possible_hints)
+
+b= Board(d)4
+b.start_game(3)
